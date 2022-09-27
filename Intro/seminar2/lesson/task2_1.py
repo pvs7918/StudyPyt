1d806e49885385e4dc74ba3b3c8a117f7a8499dd
Pavel <p_v_s@mail.ru>
2022-09-28 01:02:08 +0700

    первый коммит

---
+++ Intro/seminar2/lesson/task2_1.py
@@ -0,0 +1,4 @@
+N = int(input('Введите число: '))
+
+for i in range(5):
+    print((-3)**i)
---
+++ Intro/seminar2/lesson/task2_2.py
@@ -0,0 +1,9 @@
+n = int(input('Введите число: '))
+ls = list()
+
+for k in range(1,n+1):
+    ls.append(3*k+1)
+
+print(ls)
+
+# print(num, and=", ")  - вывод списка в виде строки через ,
