f9052204f91e8cf585f620a7377fb3acff17f2eb
Pavel <p_v_s@mail.ru>
2023-07-14 20:00:07 +0700

    обновил

--- PytDiving/seminar9/HW/task1.py
+++ PytDiving/seminar9/HW/task1.py
@@ -6,14 +6,21 @@
 # ○Декоратор, сохраняющий переданные параметры и результаты работы функции
 #  в json файл.
 from random import randint
-from typing import Callable
 import csv
 import json
 
 
+# реализация без декораторов. task1_decor.py - тоже самое по сути, но через декораторы
+
 def solving_quad_equation(a, b, c):
     d = b ** 2 - 4 * a * c
-    if d > 0:
+    if a == 0:
+        # варианты неполного квадратного уравнения
+        if b == 0:
+            return None
+        else:
+            return -c / b
+    elif d > 0:
         return (-b + d ** 0.5) / (2 * a), (-b - d ** 0.5) / (2 * a)
     elif d == 0:
         return -b
@@ -28,20 +35,22 @@ def gen_csv(cnt: int, n_min, n_max):
                                    dialect='excel',
                                    quoting=csv.QUOTE_NONNUMERIC)
         csv_write.writeheader()
+        dict = {}
         for i in range(cnt):
-            a = randint(n_min, n_max + 1)
-            b = randint(n_min, n_max + 1)
-            c = randint(n_min, n_max + 1)
-            csv_write.writerow([a, b, c])
+            dict["a"] = randint(n_min, n_max + 1)
+            dict["b"] = randint(n_min, n_max + 1)
+            dict["c"] = randint(n_min, n_max + 1)
+            csv_write.writerow(dict)
+    return True
 
 
 def read_csv_and_decide_equations():
     with open('task1.csv', 'r', newline='', encoding='utf-8') as f1:
-        csv_file = csv.reader(f1, delimiter=",")
+        csv_file = csv.reader(f1, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
         res_dict = {}
-        for row in csv_file:
-            a, b, c = row.split()
-            res_dict[a, b, c] = solving_quad_equation(a, b, c)
+        for i, row in enumerate(csv_file):
+            if i != 0:
+                res_dict[str(row)] = solving_quad_equation(row[0], row[1], row[2])
     return res_dict
 
 
@@ -49,3 +58,16 @@ def save_to_json(res_dict):
     with open('task1.json', 'w', encoding='utf-8') as f1:
         json.dump(res_dict, f1, ensure_ascii=False, indent=4)
 
+
+def main():
+    # game = outer()
+    # game()
+
+    # 1. генерация csv файла с коэффициентами a,b,c
+    gen_csv(cnt=randint(100, 1001), n_min=-10, n_max=10)
+    res_dict = read_csv_and_decide_equations()
+    save_to_json(res_dict)
+
+
+if __name__ == '__main__':
+    main()
