d87a5616112c67fc76469a4bb4d5ba27c1e0db75
Pavel <p_v_s@mail.ru>
2023-07-24 21:12:15 +0700

    выполнил ДЗ 14

--- PytDiving/seminar14/lesson/task3.py
+++ PytDiving/seminar14/lesson/task3.py
@@ -5,3 +5,33 @@
 # 📌возврат строки с удалением знаков пунктуации
 # 📌возврат строки с удалением букв других алфавитов
 # 📌возврат строки с учётом всех вышеперечисленных пунктов (кроме п. 1)
+
+from string import ascii_lowercase
+import unittest
+
+
+def removal_chars(text: str) -> str:
+    result = ''
+    for ch in text.lower():
+        if ch in ascii_lowercase + ' ':
+            result += ch
+
+    return result
+
+
+class TestFunc(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(removal_chars("wefg") == "wefg")
+
+    def test_2(self):
+        self.assertEqual(removal_chars("we,,!f.g") , "wefg")
+
+    def test_3(self):
+        self.assertFalse(removal_chars("WERtЕР") == "WERtер")
+
+    def test_4(self):
+        self.assertEqual(removal_chars("we,, !f.gЕРJ"), "we fgj")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- PytDiving/seminar14/lesson/task6.py
+++ PytDiving/seminar14/lesson/task6.py
@@ -3,10 +3,22 @@
 # 📌Напишите 3-7 тестов pytest для данного проекта.
 # 📌Используйте фикстуры.
 
+import pytest
+from square import Square
 
 
 
-class ErrorLevel(ErrorDefault):
-    
+@pytest.fixture
+def fix():
+    sq = Square(18, 7)
+    return sq
+
+def test_1(fix):
+    assert fix.perimeter() == 50
+
+def test_2(fix):
+    with pytest.raises(ValueError):
+        fix.length = -10
 
 if __name__ == "main":
+    pytest.main(["-v"])
